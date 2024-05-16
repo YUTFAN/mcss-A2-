b2f00c4dd83d0f6b3fc2bc692a193e584ceac6b7
Yutian Fan <yutfan1@student.unimelb.edu.au>
2024-05-16 16:34:15 +0800

    所有参数

---
+++ muscle_development.py
@@ -0,0 +1,27 @@
+import csv
+import random
+import math
+
+#固定的参数
+
+#合成激素的最大值和最小值
+ANABOLIC_HORMONE_MAX = 200
+ANABOLIC_HORMONE_MIN = 50
+
+#分解激素的最大值和最小值
+CATABOLIC_HORMONE_MAX = 250
+CATABOLIC_HORMONE_MIN = 52
+
+#可以改变的参数(按照netlogo中的顺序和默认值)
+#运动强度
+INTENSITY = 95
+#睡眠时间
+SLEEP = 8
+#运动间隔天数
+INTERVAL = 5
+#慢肌纤维比例
+SLOW_FIBER = 50
+#是否力量训练
+LIFT = True
+
+
